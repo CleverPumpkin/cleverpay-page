358786f0bedd11aa13c9e64f5132e1313ab4d2ec
Daniil Kozlov <d.kzlv.ru@gmail.com>
2019-07-10 17:40:20 +0300

    Making a more clear bridge implementation

--- src/lib/bridge.js
+++ src/lib/bridge.js
@@ -1,13 +1,15 @@
+const isIos = typeof webkit !== 'undefined'
+const isAndroid = typeof android !== 'undefined'
+
 export function pageSendsMessage(rawMessage) {
   var message = JSON.stringify(rawMessage)
-
-  typeof webkit !== 'undefined'
-    ? webkit.messageHandlers.pageSendsMessage.postMessage(message)
-    : android.pageSendsMessage(message)
+  if (isIos) webkit.messageHandlers.pageSendsMessage.postMessage(message)
+  else if (isAndroid) android.pageSendsMessage(message)
+  else console.log(message)
 }
 
 export function closeModal() {
-  typeof webkit !== 'undefined'
-    ? webkit.messageHandlers.closeModal.postMessage('')
-    : android.closeModal('')
+  if (isIos) webkit.messageHandlers.closeModal.postMessage('')
+  else if (isAndroid) android.closeModal('')
+  else console.log('Closing modal')
 }
